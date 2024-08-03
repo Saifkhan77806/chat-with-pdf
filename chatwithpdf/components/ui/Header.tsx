0e2daac54096cc7b8929476144fd3722f06c3ab9
saifkhan77806 <saifkhan77806@gmail.com>
2024-08-03 16:52:09 +0530

    document header 3 added on top document uploading start

--- chatwithpdf/components/ui/Header.tsx
+++ chatwithpdf/components/ui/Header.tsx
@@ -1,12 +1,30 @@
 import { SignedIn, UserButton } from "@clerk/nextjs"
+import { Button } from "@/components/ui/button"
+import {FilePlus2} from "lucide-react"
 import Link from "next/link"
 
 function Header() {
   return (
-    <div>
+    <div className="flex justify-between bg-white shadow-sm">
         <Link href="/dashboard" > Chat to <span className="text-indigo-600">PDf</span></Link>
        <SignedIn>
-        <div>
+        <div className="flex items-center space-x-2">
+
+            <Button asChild variant="link" className="hidden md:flex">
+                <Link href="/dashboard/upgrade">Pricing </Link>
+            </Button>
+
+            <Button asChild variant="outline">
+                <Link href="/dashboard">My documents</Link>
+            </Button>
+
+            <Button asChild variant="outline">
+                <Link href="/dashboard/upload">
+                <FilePlus2 className="text-indigo-600"/>
+                </Link>
+            </Button>
+
+
             <UserButton />
         </div>
        </SignedIn>
